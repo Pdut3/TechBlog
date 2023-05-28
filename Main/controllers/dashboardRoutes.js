d5697ffc1d145b02b0c93771a811bd958e12ac58
Pdut3 <115908348+Pdut3@users.noreply.github.com>
2023-05-28 11:43:00 -0600

    used most of the code from the mini project, made changes, added handlebar files

---
+++ Main/controllers/dashboardRoutes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+      
+      const postData = await Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        }
+      });
+  
+      
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      
+      res.render('dashboard', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
+  router.get('/newPost', withAuth, (req, res) => {
+      res.render('/newPost');
+    });
+
+    router.get('/edit: id', withAuth, async (req, res) => {
+        try {
+              const postData = await Post.findByPk(req.params.id, {
+                
+              });
+          
+              const post = postData.get({ plain: true });
+          
+              res.render('editPost', {
+                post,
+                logged_in: req.session.logged_in
+              });
+            } catch (err) {
+              res.status(500).json(err);
+            }
+          });
+          
+
+
+
+
+
+        res.render('/newPost');
+      
+
+
+
+
+
+  module.exports = router;
---
+++ Main/models/Comment.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    body: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+  }
+);
+
+module.exports = Comment;
