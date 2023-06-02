d6e7b14962afea32a256401dac766ce420192fc6
Pdut3 <115908348+Pdut3@users.noreply.github.com>
2023-06-02 17:40:07 -0600

    changes to the routes of views page

--- Main/controllers/api/commentRoutes.js
+++ Main/controllers/api/commentRoutes.js
@@ -2,18 +2,13 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.Comment('/', withAuth, async (_req, res) => {
+router.get('/', withAuth, async (_req, res) => {
     try {
         res.status(200).json(Comment);
     } catch (err) {
         res.status(400).json(err);
     }
 });
-
-
-
-
-
 
 
 module.exports = router;
