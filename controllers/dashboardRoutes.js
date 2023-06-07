bea35d7aecf86f184a79b3bd5f55b2d9d82a46ba
Pdut3 <115908348+Pdut3@users.noreply.github.com>
2023-06-06 18:54:45 -0600

    more spelling fixes and heroku deployed

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -23,6 +23,47 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get('/', async (req, res) => {
+    try {
+      const postData = await User.findAll({
+        where: {
+            user_id: req.session.user_id
+        }
+      });
+  
+      
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      
+      res.render('dashboard', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/', async (req, res) => {
+    try {
+      const postData = await Comment.findAll({
+        where: {
+            user_id: req.session.user_id
+        }
+      });
+  
+      
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      
+      res.render('dashboard', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
   router.get('/newPost', withAuth, (req, res) => {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,6 +69,10 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
